ed728a3fc4d23d086f1bc7121abeb865c647dd4a
Chaitanya Kumar <chaitanyakumar@srujans-macbook-pro.local>
2016-03-04 20:04:47 -0600

    Initial Commit

---
+++ SearchAcronym/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SearchAcronym
+//
+//  Created by Chaitanya Kumar on 3/4/16.
+//  Copyright © 2016 Chaitanya Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SearchAcronym/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SearchAcronym
+//
+//  Created by Chaitanya Kumar on 3/4/16.
+//  Copyright © 2016 Chaitanya Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
